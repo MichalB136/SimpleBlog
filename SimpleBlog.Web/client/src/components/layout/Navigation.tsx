be02cd98892dcad9804e929117410ef8ed3a0589
Michał Bojko <likerm13@gmail.com>
2026-01-17 22:21:20 +0100

    refactor: remove readonly modifier from onLogout prop in NavigationProps interface

--- SimpleBlog.Web/client/src/components/layout/Navigation.tsx
+++ SimpleBlog.Web/client/src/components/layout/Navigation.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '@/context/AuthContext';
 import { useCart } from '@/hooks/useCart';
 
 interface NavigationProps {
-  readonly onLogout: () => void;
+  onLogout: () => void;
 }
 
 export function Navigation({ onLogout }: Readonly<NavigationProps>) {
